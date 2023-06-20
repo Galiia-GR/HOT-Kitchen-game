7f3784473419d5bb4ff0f9070f76fec34c54e062
Galiia-GR <hope05590@gmail.com>
2023-06-20 21:49:14 +0500

    feat: start task

---
+++ RSS-CSS-Selectors/src/levelList.ts
@@ -0,0 +1,10 @@
+export const levels = [
+    {
+        level: 0,
+        title: 'Первый уровень',
+        history: 'Делай делай уровень',
+        help: 'Делай сам',
+        selector: 'bun',
+        showHTML: `<bun></bun>`,
+    },
+];
