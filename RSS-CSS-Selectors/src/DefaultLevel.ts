5df860b4279b2cedb97d66b7f4b94d5a3ebc5b31
Galiia-GR <hope05590@gmail.com>
2023-06-27 23:02:45 +0500

    feat: add input function and win function

--- RSS-CSS-Selectors/src/DefaultLevel.ts
+++ RSS-CSS-Selectors/src/DefaultLevel.ts
@@ -1,5 +1,7 @@
 const showHtml = document.querySelector('.editor-html__markup');
 const showLevel = document.querySelector('.levels-show');
+const arrBoardPictires = document.querySelectorAll('.bun');
+const arrLevels = document.querySelectorAll('.levels__item');
 
 interface DefaultLevel {
     level: number;
@@ -7,8 +9,9 @@ interface DefaultLevel {
     history: string;
     help: string;
     tag: string;
-    animation: boolean;
+    id: string;
     html: string[];
+    child: string[];
 }
 
 class DefaultLevel implements DefaultLevel {
@@ -18,16 +21,18 @@ class DefaultLevel implements DefaultLevel {
         history: string,
         help: string,
         tag: string,
-        animation: boolean,
-        html: string[]
+        id: string,
+        html: string[],
+        child: string[]
     ) {
         this.level = level;
         this.title = title;
         this.history = history;
         this.help = help;
         this.tag = tag;
-        this.animation = animation;
+        this.id = id;
         this.html = html;
+        this.child = child;
     }
 
     createAppendLevel() {
@@ -44,26 +49,70 @@ class DefaultLevel implements DefaultLevel {
     }
 
     createHtmlMarkUp() {
-        const [temp0, temp1, temp2, temp3] = this.html;
-        const html0 = document.createElement('div');
-        html0.classList.add('markup__item');
-        showHtml?.appendChild(html0);
-        html0.textContent = temp0;
+        const [...arrHtml] = this.html;
 
-        const html1 = document.createElement('div');
-        html1.classList.add('markup__item');
-        showHtml?.appendChild(html1);
-        html1.textContent = temp1;
+        arrHtml.forEach((element, index: number) => {
+            const tempDiv = document.createElement('div');
+            tempDiv.classList.add('markup__item');
 
-        const html2 = document.createElement('div');
-        html2.classList.add('markup__item');
-        showHtml?.appendChild(html2);
-        html2.textContent = temp2;
+            switch (index) {
+                case 0:
+                    tempDiv.setAttribute('data', `${element}First`);
+                    showHtml?.appendChild(tempDiv);
+                    break;
 
-        const html3 = document.createElement('div');
-        html3.classList.add('markup__item');
-        showHtml?.appendChild(html3);
-        html3.textContent = temp3;
+                case 1:
+                    tempDiv.setAttribute('data', `${element}First`);
+                    showHtml?.appendChild(tempDiv);
+                    break;
+
+                case 2:
+                    tempDiv.setAttribute('data', `${element}Second`);
+                    showHtml?.appendChild(tempDiv);
+                    break;
+                case 3:
+                    tempDiv.setAttribute('data', `${element}Second`);
+                    showHtml?.appendChild(tempDiv);
+                    break;
+                default:
+                    console.log('Opps');
+                    break;
+            }
+            const tempSpan = document.createElement('span');
+            tempDiv?.appendChild(tempSpan);
+            if (typeof element === 'string' && this.id !== '' && index === 1) {
+                tempSpan.textContent = `<${element} id=${this.id}>
+                </${element}>`;
+                tempDiv.classList.add(`${element}`);
+            } else {
+                tempSpan.textContent = `<${element}>
+                </${element}>`;
+                tempDiv.classList.add(`${element}`);
+            }
+        });
+    }
+
+    createHintsShakeElements() {
+        console.log('this шейк');
+        arrBoardPictires.forEach((element) => {
+            if (element.classList.contains(this.tag)) element.classList.add('shake');
+        });
+    }
+
+    win() {
+        const button = document.querySelector('.editor-css__button');
+        const inputPlace = document.querySelector('.editor-css__input') as HTMLInputElement;
+        button?.addEventListener('click', () => {
+            if (inputPlace) {
+                if (this.tag === inputPlace.value) {
+                    arrLevels.forEach((item) => {
+                        if (Number(item.getAttribute('id')) === this.level) {
+                            item.classList.add('win');
+                        }
+                    });
+                }
+            }
+        });
     }
 }
 
--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -4,6 +4,10 @@ import DefaultLevel from './DefaultLevel';
 
 const showHtml = document.querySelector('.editor-html__markup');
 const showLevel = document.querySelector('.levels-show');
+const arrBoardPictires = document.querySelectorAll('.bun');
+const htmlMarkUp = document.querySelector('.editor-html__markup');
+const arrLevels = document.querySelectorAll('.levels__item');
+const dataLevel: number[] = [];
 
 let currentLevel: number = levelsList[0].level;
 
@@ -14,37 +18,117 @@ function drawLevel(level: number) {
         levelsList[level].history,
         levelsList[level].help,
         levelsList[level].tag,
-        levelsList[level].animation,
-        levelsList[level].html
+        levelsList[level].id,
+        levelsList[level].html,
+        levelsList[level].child
     );
     myObj.createAppendLevel();
     myObj.createHtmlMarkUp();
+    myObj.createHintsShakeElements();
+    myObj.win();
 }
 
 drawLevel(currentLevel);
 
 function clearDrawLevel() {
-    if (showHtml && showLevel) {
+    if (showHtml && showLevel && arrBoardPictires) {
         showHtml.innerHTML = '';
         showLevel.innerHTML = '';
+        arrBoardPictires.forEach((element) => {
+            if (element.classList.contains('shake')) element.classList.remove('shake');
+        });
     }
 }
 
-console.log('я попадаю сюда');
-
-console.log('пам пам пам');
+console.log('я попадаю сюда пам пам пам и нажимаю уровни');
 
 const levels = document.querySelectorAll('.levels-container');
+const inputPlace = document.querySelector('.editor-css__input') as HTMLInputElement;
 
 levels.forEach((el) =>
     el.addEventListener('click', (e) => {
         const temp = e.target;
-        console.log(e.target);
 
         if ((temp as HTMLElement)?.getAttribute('id')) {
             currentLevel = Number((temp as HTMLElement)?.getAttribute('id'));
+            inputPlace.value = '';
             clearDrawLevel();
+
             drawLevel(currentLevel);
         }
     })
 );
+
+console.log(htmlMarkUp);
+
+htmlMarkUp?.addEventListener('mouseover', (e) => {
+    const targetNode = e.target as Node;
+    if (targetNode.parentNode) {
+        const parentElement = targetNode.parentNode as HTMLElement;
+        const dataHtml = parentElement.getAttribute('data');
+
+        console.log(dataHtml);
+
+        arrBoardPictires.forEach((el) => {
+            if (dataHtml) {
+                if (el.parentElement?.classList.contains(dataHtml)) {
+                    el.parentElement?.classList.add('hover');
+                }
+            }
+        });
+    }
+});
+
+htmlMarkUp?.addEventListener('mouseout', (e) => {
+    const targetNode = e.target as Node;
+    if (targetNode.parentNode) {
+        const parentElement = targetNode.parentNode as HTMLElement;
+        const dataHtml = parentElement.getAttribute('data');
+
+        console.log(dataHtml);
+
+        arrBoardPictires.forEach((el) => {
+            if (dataHtml && el.parentElement?.classList.contains(dataHtml)) {
+                el.parentElement?.classList.remove('hover');
+            }
+        });
+    }
+});
+
+const checkWin = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+        if (mutation.attributeName === 'class') {
+            const targetElement = mutation.target as HTMLElement;
+            if (targetElement.classList.contains('win')) {
+                dataLevel.push(Number(targetElement.getAttribute('id')));
+                localStorage.setItem('dataLevel', JSON.stringify(dataLevel));
+                clearDrawLevel();
+                currentLevel = Number(targetElement.getAttribute('id')) + 1;
+                console.log(currentLevel);
+
+                drawLevel(currentLevel);
+            }
+        }
+    });
+});
+
+arrLevels.forEach((element) => {
+    checkWin.observe(element, { attributes: true });
+});
+
+function winStore() {
+    const getLevel = localStorage.getItem('dataLevel');
+    if (getLevel) {
+        const levelData = JSON.parse(getLevel);
+        console.log(levelData);
+        arrLevels.forEach((item) => {
+            levelData.forEach((el: number) => {
+                if (Number(item.getAttribute('id')) === el) {
+                    item.classList.add('win');
+                }
+            });
+        });
+    }
+}
+
+winStore();
